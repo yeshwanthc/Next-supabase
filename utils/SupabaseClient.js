36e6e423f19fb2fe00a29e918fe5daa917467ec2
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2024-03-14 13:12:29 +0530

    Update SupabaseClient.js

--- utils/SupabaseClient.js
+++ utils/SupabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://keodgmfziankdgtebpao.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlb2RnbWZ6aWFua2RndGVicGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzMzU5MTMsImV4cCI6MjAyNTkxMTkxM30.qXD1M9L0iq7GSrcpjjjJMszFdNNZIMIe4AhczINSqX0";
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
